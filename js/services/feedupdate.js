d1a9e46f7f3b5610bfc52450f161319acae5a216
Dina Nashaat <dina.nashaat@gmail.com>
2017-06-25 00:47:47 +0200

    change request type

--- js/services/feedupdate.js
+++ js/services/feedupdate.js
@@ -2,5 +2,5 @@ app.factory('feedupdate', ['$http', function($http) {
     
     
     var url = 'http://feeds.feedburner.com/raymondcamdensblog';
-    return $http.get(url).success(function(data){return data}).error(function(err){return err});
+    return $http.jsonp(url).success(function(data){return data}).error(function(err){return err});
 }]);
