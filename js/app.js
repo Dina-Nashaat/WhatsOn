9a7e90baedf7de30940c0dec139213fad3f4699a
Dina Nashaat <dina.nashaat@gmail.com>
2017-06-24 14:29:50 +0200

    Init Project

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var app = angular.module("myApp",[]);
+
+ $(document).ready(function() {
+    $('select').material_select();
+ });
+  
---
+++ js/directives/feedUpdate.js
@@ -0,0 +1,6 @@
+app.directive('feedUpdate', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'js/directives/appInfo.html'
+    };
+});
